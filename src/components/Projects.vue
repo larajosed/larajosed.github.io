6ebbd431662a41ffc149dfa0e6e79bede62b43a8
larajosed <larajosed@gmail.com>
2023-02-25 15:50:03 +0100

    Adding icons in the technology component

---
+++ src/components/Projects.vue
@@ -0,0 +1,3 @@
+<template>
+    <h3>Proyectos</h3>
+   </template>
---
+++ src/components/Technologies.vue
@@ -0,0 +1,47 @@
+<template>
+    <section>
+        <div>
+            <h3>Tecnologias</h3>
+        </div>
+        <div>
+            <div>Frontend
+                <div class="icon-container">
+                    <img class="icon" src="../assets/images/frontend/angular.png" alt="ANGULAR">
+                    <img class="icon" src="../assets/images/frontend/bootstrap.png" alt="BOOTSTRAP">
+                    <img class="icon" src="../assets/images/frontend/vue.png" alt="VUE">
+                    <img class="icon" src="../assets/images/frontend/d3.png" alt="D3">
+                </div>
+            </div>
+            <div>Backend
+                <div class="icon-container">
+                    <img class="icon" src="../assets/images/backend/ExpressJS.png" alt="EXPRESSJS">
+                    <img class="icon" src="../assets/images/backend/nodejs.png" alt="NODEJS">
+                    <img class="icon" src="../assets/images/backend/spring.png" alt="SPRING">
+                    <img class="icon" src="../assets/images/backend/c.png" alt="C#">
+                </div>
+            </div>
+            <div>BBDD
+                <div class="icon-container">
+                    <img class="icon" src="../assets/images/bbdd/mysql.png" alt="MYSQL">
+                    <img class="icon" src="../assets/images/bbdd/MongoDB.png" alt="MONGODB">
+                </div>
+            </div>
+            <div>Otros
+                <div class="icon-container">
+                    <img class="icon" src="../assets/images/otros/bamboo.png" alt="BAMBOO">
+                    <img class="icon" src="../assets/images/otros/Bitbucket.png" alt="BITBUCKET">
+                    <img class="icon" src="../assets/images/otros/JIRA.png" alt="JIRA">
+                    <img class="icon" src="../assets/images/otros/nexus.png" alt="NEXUS">
+                    <img class="icon" src="../assets/images/otros/sourcetree.png" alt="SOURCETREE">
+                </div>
+            </div>
+        </div>
+    </section>
+</template>
+<style scoped>
+.icon {
+   height: 4em;
+   width: 4em;
+   margin-right: 1em
+}
+</style>
