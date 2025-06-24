d5a27ad5a102dcf60cb7b79f2f0d1999fd8ddb4f
José Daniel Lara <larajosed@gmail.com>
2025-06-24 17:08:43 +0200

    Adding Google Analytics measurement id configuration

--- src/main.js
+++ src/main.js
@@ -1,10 +1,19 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import './assets/main.css';
+import { createApp } from "vue";
+import App from "./App.vue";
+import "./assets/main.css";
 import router from "./router";
+import { configure, createGtag } from "vue-gtag";
 
-const app = createApp(App)
-app.use(router)
+configure({
+  tagId: "G-HBQTGJQJH8",
+});
+
+const gtag = createGtag({
+  tagId: "G-HBQTGJQJH8",
+});
+
+const app = createApp(App);
+app.use(router);
+app.use(gtag);
 
 app.mount("#app");
-
