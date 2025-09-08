6ef406399d9fc11afd1658cbd9e62019560dd878
José Daniel Lara <larajosed@gmail.com>
2025-09-08 19:13:31 +0200

    delete experience deimos

--- src/components/Experience.vue
+++ src/components/Experience.vue
@@ -22,18 +22,9 @@ export default {
           logo: "images/aspasia.png",
           title: "Desarrollador Full Stack Jr - Grupo Aspasia.",
           mainParagraph: [
-            "Como desarrollador full stack, participé en varios proyectos dentro del sector de la formación para empresas, organizaciones e instituciones. Mis responsabilidades incluyeron el mantenimiento y desarrollo de nuevas funcionalidades en una herramienta de gestión de formación para empresas y la implementación de mejoras en diversas aplicaciones. Para el frontend, utilicé jQuery, y para el backend, PHP.",
+            "Toma y análisis de requisitos funcionales para propuesta de nuevos desarrollos en una herramienta de gestión de formación en empresas para cliente BENTELER Group. Implementación de las soluciones propuestas al clientes. Tecnologías: JQuery y PHP. Exportación de datos en formato xlsx (Excel) para su carga en FUNDAE.",
           ],
           date: "Jun. 2023 - Dic. 2023.",
-          country: "Madrid, España.",
-        },
-        {
-          logo: "images/deimos.png",
-          title: "Desarrollador Full Stack Jr - Elecnor Deimos.",
-          mainParagraph: [
-            "En mi experiencia como desarrollador full stack, participé en varios proyectos, enfocándome principalmente en el desarrollo de aplicaciones para el frontend. Utilicé Angular y Angular Material, y fui responsable tanto de la resolución de incidencias como de la creación de nuevas funcionalidades. Para la visualización de datos complejos, como gráficas y esquemas de conexión, integré la potente librería D3.js en las aplicaciones.",
-          ],
-          date: "Jun. 2022 - Nov. 2022",
           country: "Madrid, España.",
         },
       ],
